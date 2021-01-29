96957905af134792d929166458f058ace3a08706
numidiasoft <tarik.ihadjadene@sendinblue.com>
2021-01-29 15:42:17 +0100

    setup jest badge project

--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,12 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import fs from 'fs'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const coverageResult: string = core.getInput('coverage-json-file')
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    const content = fs.readFileSync(coverageResult, {encoding: 'utf8'})
+    core.info(content)
   } catch (error) {
     core.setFailed(error.message)
   }
